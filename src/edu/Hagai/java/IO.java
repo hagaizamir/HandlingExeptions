012fec76d2ca13a856c38add8c7994ebf7b26d27
hagaizamir <hagai34@gmail.com>
2017-03-14 21:24:04 +0200

    file write

--- src/edu/Hagai/java/IO.java
+++ src/edu/Hagai/java/IO.java
@@ -1,11 +1,26 @@
 package edu.Hagai.java;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
  * Created by Tomer on 14/03/2017.
  */
 public class IO {
+    public  static  boolean write7 (String path , String data){
+        try(FileWriter writer = new FileWriter(path,true)){
+            writer.write(data);//security problem -> IO fails
+            return  true;
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return  false;
+    }
+
+
     public static int getInteger(String prompt){
         Scanner scanner = new Scanner(System.in);
 
